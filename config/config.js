fef5689cebac1222a72526986008fab9b2645230
Michael Lynn <merlynn@gmail.com>
2019-12-20 20:36:25 -0500

    switch to cloud mongodb

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://localhost:27017/ghsdp',
-  db_dev: 'mongodb://localhost:27017/ghsdp',
+  db: 'mongodb+srv://mike:Password123@performance-zbcul.mongodb.net/test?retryWrites=true&w=majority',
+  db_dev: 'mongodb+srv://mike:Password123@performance-zbcul.mongodb.net/test?retryWrites=true&w=majority',
 };
